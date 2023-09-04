e787ef2b7c6b2864836a0b49672693d0a4e876e1
super <evanarendra7@gmail.com>
2023-09-04 23:50:18 +0700

    first commit

---
+++ popup.js
@@ -0,0 +1,27 @@
+// alert('this is alert'); 
+
+// var name = prompt('what is ur name : ');
+// alert('hello ' + name);
+
+// var test = confirm('are you BMTH fans?');
+
+// if (test === true){
+//     alert('Sempiternal');
+// }else{
+//     alert('you suck!');
+// }
+
+alert('welcome');
+
+var again = true;
+
+while(again){
+    var name = prompt('input your name : ');
+    alert('hola ' + name);
+
+    again = confirm('try again?');
+}
+
+alert('Thank you');
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+// save variable x on page
+var x = 15;
+console.log('this variable is ' + x);
