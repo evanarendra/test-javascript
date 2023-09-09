81a171fa6b5cc15b1ef0d7bda385472f1dd67b13
super <evanarendra7@gmail.com>
2023-09-10 03:24:10 +0700

    array 1

---
+++ array.js
@@ -0,0 +1,26 @@
+// add Array
+
+var days = ['Monday','Tuesday','Wednesday','Thursday'];
+console.log(days[2]); //show array index 2 
+
+var vechile = [];
+vechile[0] = 'Bus';
+vechile[1] = 'Car';
+vechile[2] = 'Motorcycle';
+vechile[5] = 'Boat';
+
+console.log(arr1);
+
+// delete Array
+
+var employee = ['Steve','Jacob','Maddison','David'];
+employee[1] = undefined;
+console.log(employee);
+
+// show value Array
+
+var value = ['Steve','Jacob','Maddison','David'];
+
+for(var i = 0; i < value.length; i++){
+    console.log(value[i]);
+}
