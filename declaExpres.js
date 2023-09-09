5a429403559368146074d1c33f665bc1d626d486
super <evanarendra7@gmail.com>
2023-09-10 03:12:11 +0700

    declaration expression

---
+++ declaExpres.js
@@ -0,0 +1,14 @@
+// function declaration & expression
+
+// declaration
+function showMessage(name){
+    alert('halo ' + name);
+}
+
+showMessage('Sonia');
+
+//expression
+
+var showMessage1 = function(name){
+    alert('halo ' + name);
+}
