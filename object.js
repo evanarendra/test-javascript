85368a64dcf45e820cfba6ba0a74c70879c86032
super <evanarendra7@gmail.com>
2023-09-17 19:39:04 +0700

    create object array

---
+++ object.js
@@ -0,0 +1,11 @@
+// create object
+var student = {
+    name :  "Evans",
+    age  : 26,
+    gpa : [2.6, 2.9, 3.4],
+    address : {
+        resident : "Timoho",
+        city : "Yogyakarta",
+        province : "D.I.Y"
+    }
+};
