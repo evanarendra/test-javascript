69b7d7943f8d7747179f69e5f63f9d1cc32aa162
super <evanarendra7@gmail.com>
2023-09-17 20:07:32 +0700

    declaration & constructor

--- object.js
+++ object.js
@@ -7,5 +7,38 @@ var student = {
         resident : "Timoho",
         city : "Yogyakarta",
         province : "D.I.Y"
-    }
+    }   
 };
+
+var student2 = {
+    name :  "John",
+    age  : 22,
+    gpa : [2.5, 3.9, 3.4],
+    address : {
+        resident : "Wirobrajan",
+        city : "Yogyakarta",
+        province : "D.I.Y"
+    }   
+};
+
+// Declaration
+function createStudentObject(name, number, email, major){
+    var student = {};
+    student.name = name;
+    student.number = number;
+    student.email = email;
+    student.major = major;
+    return student;
+}
+
+var student3 = createStudentObject('Paul', '12340', 'paul@gmail.com','IT');
+
+// Constructor
+function Students(name, number, email, major){
+    this.name = name;
+    this.number = number;
+    this.email = email;
+    this.major = major;
+}
+
+var student4 = new Students('Stones', '42340', 'stones@gmail.com','Business')
